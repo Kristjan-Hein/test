0300b382ad2989b887872ee068f3e3f134e984fb
Kristjan-Hein <59916644+Kristjan-Hein@users.noreply.github.com>
2021-09-14 09:54:22 +0300

    Add files via upload

---
+++ sss.js
@@ -0,0 +1,11 @@
+const test1 = 123;
+const test2 = 123;
+const test3 = 123;
+test4 = 123;
+console.log(test1, test2, test3, test4);
+
+function hello(){
+    return 'hello world';
+}
+
+console.log(hello());
